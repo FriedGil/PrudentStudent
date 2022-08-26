1470d0054ff88749595a59cfa545efd3bf14de23
warren yun <wyun2006@gmail.com>
2022-08-26 19:48:55 -0400

    auth lucia

--- my-app/src/lib/lucia.ts
+++ my-app/src/lib/lucia.ts
@@ -1,2 +1,10 @@
 import lucia from "lucia-sveltekit";
+import prisma from "@lucia-sveltekit/adapter-prisma";
+import { dev } from "$app/env";
+
+export const auth = lucia({
+  adapter: prisma(prisma),
+  secret: import.meta.env.SECRET,
+  env: dev ? "DEV" : "PROD"
+});
 
