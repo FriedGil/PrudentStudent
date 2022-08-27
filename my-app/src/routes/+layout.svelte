c3ce88bf053db17a496b680022f857d6b0a505c4
TheRoyalEgg <76929023+TheRoyalEgg@users.noreply.github.com>
2022-08-27 23:48:39 +0000

    pru button component

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import '../app.css';
   import Nav from '$lib/components/Nav.svelte';
   import Footer from '$lib/components/Footer.svelte';
+  import Pru from '$lib/components/Pru.svelte';
 
   import type { LayoutServerData } from "./$types"
   export let data: LayoutServerData
@@ -13,4 +14,8 @@
   <Nav userData={data} />
 	<slot />
   <Footer />
+  <Pru />
+  
 </Lucia>
+
+
