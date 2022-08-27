762156eccc04ff87ac15b3b986d47bd188dacf31
TheRoyalEgg <76929023+TheRoyalEgg@users.noreply.github.com>
2022-08-27 17:25:42 +0000

    classes page

--- my-app/src/routes/about/+page.svelte
+++ my-app/src/routes/about/+page.svelte
@@ -1,3 +1,3 @@
-<div class="hero min-h-screen bg-neutral">
+<div class="hero min-h-screen bg-base-200">
     <p class="py-6 ">Despite an increasing shift towards using web technology for student administration, existing technologies are either bloated, backed by morally reprehensive big tech companies, or both.  </p>
   </div>
