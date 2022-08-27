7d1c3d608fdebf6033e58627895b22fd647443c5
AngleSideAngle <asapaparo@gmail.com>
2022-08-26 22:55:24 -0400

    temp

---
+++ my-app/src/routes/classes/+page.server.ts
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit';
+import { PrismaClient } from '@prisma/client';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+    const post = await getPostFromDatabase(params.slug);
+
+    if (post) {
+        return post;
+    }
+
+    throw error(404, 'Not found');
+}
