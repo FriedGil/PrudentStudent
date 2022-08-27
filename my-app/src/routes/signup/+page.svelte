7462694a6c5a389ad2005f5010868b61575428e5
warren yun <wyun2006@gmail.com>
2022-08-26 23:06:34 -0400

    link login

--- my-app/src/routes/signup/+page.svelte
+++ my-app/src/routes/signup/+page.svelte
@@ -7,46 +7,47 @@
 	};
 </script>
 
-<div class="hero min-h-screen bg-base-200 flex px-20">
+<div class="hero min-h-screen bg-base-200 flex justify-center px-20">
 	<div class="hero-content flex-col lg:flex-row-reverse">
 		<div class="ml-6 text-center lg:text-left">
 			<h1 class="text-5xl font-bold">Sign Up!</h1>
 			<p class="py-6">Sign up now to join our revolution against the edutech establishment.</p>
+			<p class="text-md">Already have an account? Log in <a href="/login" class="text-purple-500">here!</a></p>
 		</div>
 		<div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
 			<div class="card-body">
 				<form on:submit|preventDefault={signup} action="/signup" method="post">
-          <div class="form-control">
-            <label class="label" for="email">
-              <span class="label-text">Email</span>
-            </label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              placeholder="Type here"
-              class="input input-ghost w-full max-w-xs"
-              bind:value={email}
-            />
-          </div>
-          <div class="form-control">
-            <label class="label" for="password">
-              <span class="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Type here"
-              class="input input-ghost w-full max-w-xs"
-              bind:value={password}
-            />
-          </div>
-          <div class="form-control mt-6 mb-4">
-            <button type="submit" class="btn btn-primary">Sign up</button>
-          </div>
-        </form>
-				<p class="text-red-600">{errors?.message || ""}</p>
+					<div class="form-control">
+						<label class="label" for="email">
+							<span class="label-text">Email</span>
+						</label>
+						<input
+							type="text"
+							id="email"
+							name="email"
+							placeholder="Type here"
+							class="input input-ghost w-full max-w-xs"
+							bind:value={email}
+						/>
+					</div>
+					<div class="form-control">
+						<label class="label" for="password">
+							<span class="label-text">Password</span>
+						</label>
+						<input
+							type="password"
+							id="password"
+							name="password"
+							placeholder="Type here"
+							class="input input-ghost w-full max-w-xs"
+							bind:value={password}
+						/>
+					</div>
+					<div class="form-control mt-6 mb-4">
+						<button type="submit" class="btn btn-primary">Sign up</button>
+					</div>
+				</form>
+				<p class="text-red-600">{errors?.message || ''}</p>
 			</div>
 		</div>
 	</div>
