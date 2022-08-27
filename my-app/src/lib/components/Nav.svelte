114140d31c315bf52791daa6c2fed89aa0a9205a
warren yun <wyun2006@gmail.com>
2022-08-27 12:36:10 -0400

    move footer and nav into components

---
+++ my-app/src/lib/components/Nav.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { Icon, Home, ViewList, Logout } from "svelte-hero-icons";
+</script>
+
+<div class="navbar bg-primary text-primary-content">
+  <div class="dropdown">
+    <div tabindex="0" class="btn btn-ghost btn-circle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h7"
+        /></svg
+      >
+    </div>
+    <ul
+      tabindex="0"
+      class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-primary  rounded-box w-52"
+    >
+      <li><a href="/">Home <Icon src="{Home}" size="20" class="ml-auto inline" /> </a> </li>
+      <li><a href="/about">About <Icon src="{ViewList}" size="20" class="ml-auto inline" /></a></li>
+      <li><a href="/about">Log out <Icon src="{Logout}" size="20" class="ml-auto inline" /></a></li>
+    </ul>
+  </div>
+</div>
