cded7769acf768cd96377660a2f23203a6fd48ed
TheRoyalEgg <76929023+TheRoyalEgg@users.noreply.github.com>
2022-08-27 22:16:23 +0000

    meet pru

---
+++ my-app/src/routes/meetpru/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import bird from '$lib/assets/bird.png';
+  </script>
+  
+  <div class="hero min-h-screen" style="background-image: url({bird});">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Meet Pru</h1>
+        <p class="mb-5">Pru is your new favorite AI Assistant and the Prudent Student logo! Click on the Pru icon on the bottom left to give it a try.</p>
+      </div>
+    </div>
+  </div>
