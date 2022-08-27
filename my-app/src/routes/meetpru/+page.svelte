d2acc294828d7af821fbe6bf56a4c1ba835702a2
TheRoyalEgg <76929023+TheRoyalEgg@users.noreply.github.com>
2022-08-27 22:37:54 +0000

    pru updates

--- my-app/src/routes/meetpru/+page.svelte
+++ my-app/src/routes/meetpru/+page.svelte
@@ -7,7 +7,7 @@
     <div class="hero-content text-center text-neutral-content">
       <div class="max-w-md">
         <h1 class="mb-5 text-5xl font-bold">Meet Pru</h1>
-        <p class="mb-5">Pru is your new favorite AI Assistant and the Prudent Student logo! Click on the Pru icon on the bottom left to give it a try.</p>
+        <p class="mb-5">Pru is both your new favorite AI Assistant and also the Prudent Student logo! Click on the Pru icon at the bottom left to give it a try.</p>
       </div>
     </div>
   </div>
