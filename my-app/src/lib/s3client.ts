100e2cc659541ca8743ad1fdd89422bffb43df88
warren yun <wyun2006@gmail.com>
2022-08-28 00:28:18 -0400

    create s3 client

---
+++ my-app/src/lib/s3client.ts
@@ -0,0 +1,10 @@
+import { S3 } from "@aws-sdk/client-s3";
+
+export const s3Client = new S3({
+  endpoint: import.meta.env.VITE_DO_SPACES_URL as string,
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: import.meta.env.VITE_DO_SPACES_ID as string,
+    secretAccessKey: import.meta.env.VITE_DO_SPACES_SECRET as string
+  }
+})
